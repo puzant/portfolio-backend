ae190dca701ffe9c8a07eb29bb28283e29049a8f
Puzant <pbakjejian@hotmail.com>
2024-12-30 20:36:51 +0400

    configure cron job to run every 13 mins

---
+++ cron.js
@@ -0,0 +1,28 @@
+import { CronJob } from 'cron'
+import https from 'https'
+
+const render_backend_URL = 'https://portfolio-backend-gq2s.onrender.com/api/travel-images'
+
+const onComplete = () => {
+  console.log('Cron job has completed')
+}
+
+export const wakeupJob = new CronJob('*/13 * * * *', () => {
+  console.log('Pinging Server.....')
+
+  https.get(render_backend_URL, (res) => {
+    const chunks = []
+
+    res.on('data', (chunk) => {
+      chunks.push(chunk)
+    });
+
+    res.on('end', () => {
+      const body = Buffer.concat(chunks).toString();
+      if (res.statusCode === 200) console.log('server pinged!', body)
+        else console.error(`failed to ping the server ${res.statusCode}`)
+    })   
+  }).on('error', (err) => {
+    console.error(`Error pinging server: ${err.message}`);
+  })
+}, onComplete, true)
