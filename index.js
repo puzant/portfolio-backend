5b5dfc1d5682bd1d56ed5978d565f4a665b5fa6e
Puzant <pbakjejian@hotmail.com>
2025-01-03 12:29:57 +0400

    create template to view projects - publications

--- index.js
+++ index.js
@@ -36,6 +36,40 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
+app.get('/cms', (req, res) => {
+  res.render('index', {
+    projects: [
+      {name: 'Locus', description: 'Real-Estate Management Platform', link: 'https://www.locus.eu/', tech: ['React', 'Tailwind', 'React Query', 'Formik']},
+      {name: 'TMDB Clone', description: 'Explore And Search Movies', link: 'https://react-app-movies-tracker.netlify.app/', tech: ['React', 'Tailwind', 'React Query', 'TypeScript', ]},
+      {name: 'Puzant CV', description: 'Interactive Online CV', link: 'https://puzant-cv.netlify.app/', tech: ['AlpineJs', 'JavaScript', 'HTML', 'CSS']},
+      {name: 'Password Generator', description: 'Complex Password Generator', link: 'https://complex-password-generator-app.netlify.app/', tech: ['JavaScript', 'HTML', 'Tailwind']},
+    ],
+    publications: [
+      {
+        title: 'Efficient Data Fetching and Mutation in React with Generic Hooks and HOCs',
+        preview: 'In the ever-evolving landscape of web development, one constant remains: the need to interact with APIs. Whether you’re building web…',
+        publishedDate: 'Sep 5, 2023',
+        duration: '4',
+        link: 'https://medium.com/@puzant24/efficient-data-fetching-and-mutation-in-react-with-generic-hooks-and-hocs-37728b444ac8'
+      },
+      {
+        title: 'Building A Simple Images Carousel With ReactJs',
+        preview: 'Carousel allows multiple image & videos to be displayed in a nice & interactive way',
+        publishedDate: 'Feb 3, 2021',
+        duration: '4',
+        link: 'https://medium.com/@puzant24/building-a-simple-images-carousel-with-reactjs-377256bedc61'
+      },
+      {
+        title: 'Infinite Scroll With ReactJs & Redux',
+        preview: 'Infinite scroll has been widely used in today’s web & mobile apps, it simply loads data as you scroll down the page, this eliminated the…',
+        publishedDate: 'Jul 8, 2020',
+        duration: '3',
+        link: 'https://medium.com/@puzant24/infinite-scroll-with-reactjs-redux-23bccea01dd0'
+      }
+    ]
+  });
+});
+
 app.get('/api/travel-images', async (req, res) => {
   try {
     const cachedImages = cache.get('travelImages')
