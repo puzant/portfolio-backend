d6773149f41003e6bd0630c3290bde42fc4af7ac
Puzant <pbakjejian@hotmail.com>
2025-01-08 21:35:10 +0400

    update publications route

--- routes/publicationsRoute.js
+++ routes/publicationsRoute.js
@@ -13,7 +13,7 @@ const router = express.Router()
 router.get('/api', getAllPublications)
 router.post('/api/add', addPublication)
 router.post('/api/edit/:id', editPublication)
-router.delete('api/delete/:id', deletePublication)
+router.delete('/api/delete/:id', deletePublication)
 
 // rendering routes
 router.get('/add', renderAddPublication)
