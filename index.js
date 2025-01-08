c078851f6a5287479c4ac02e5a142a176ab604e9
Puzant <pbakjejian@hotmail.com>
2025-01-08 21:35:38 +0400

    update main file

--- index.js
+++ index.js
@@ -24,8 +24,8 @@ const __dirname = path.dirname(__fileName)
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
-app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 app.use(morgan('dev'))
 
 app.use(cors({
