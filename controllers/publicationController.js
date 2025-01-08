7e8b79125334818a59fdc0d6b0464423297db049
Puzant <pbakjejian@hotmail.com>
2025-01-08 11:57:35 +0400

    update controller

--- controllers/publicationController.js
+++ controllers/publicationController.js
@@ -1,4 +1,4 @@
-import express from 'express'
+import { DateTime } from 'luxon'
 import Publication from '../models/publications.js'
 
 export const getAllPublications = async (req, res) => {
@@ -11,17 +11,27 @@ export const getAllPublications = async (req, res) => {
   }
 }
 
-export const addPublication = async (req, res) => {}
+export const addPublication = async (req, res) => {  
+  const { title, publishedDate, link, duration, preview } = req.body
+  console.log("🚀 ~ addPublication ~ req.body:", req.body)
+  
+  try {
+    const newPublication = Publication.create({ title, publishedDate: new Date(publishedDate), link, duration, preview })
+
+    res.status(200).json({message: 'success', publication: newPublication})
+  } catch (err) {
+    res.status(500).json({ error: 'Internal server error' })
+  }
+}
 
 export const editPublication = async (req, res) => {
   const { id } = req.params
   const { title, publishedDate, link, duration, preview } = req.body
-  const formattedPublishedDate = new Date(publishedDate)
 
   try {
     const updatedPublication = await Publication.findByIdAndUpdate(
       id,
-      { title, formattedPublishedDate, link, duration, preview },
+      { title, publishedDate: new Date(publishedDate), link, duration, preview },
       { new: true, runValidators: true }
     )
 
@@ -39,16 +49,48 @@ export const editPublication = async (req, res) => {
   }
 }
 
-export const deletePublication = async (req, res) => {}
+export const deletePublication = async (req, res) => {
+  try {
+    const publicationToDelete = await Publication.findByIdAndDelete(req.params.id)
+
+    if (!publicationToDelete) 
+      return res.status(404).json({ message: 'Publication not found' })
+    res.status(200).json({
+      message: 'Publication deleted successfully', 
+      publicationToDelete
+    })
+
+      
+  } catch (error) {
+    console.error('Error deleting publication:', error);
+    res.status(500).json({ message: 'Server error', error });
+  }
+}
+
+export const renderAddPublication = async (req, res) => {
+  res.render('publication/addPublication', {
+    title: 'Add Publication'
+  })
+}
 
 export const renderEditPublication = async (req, res) => {
   try {
     const publication = await Publication.findById(req.params.id)
+    
      if (!publication) 
       return res.status(404).send('Project not found')
      
-     res.render('editPublication', { publication, title: 'Edit Publication' })
-
+     res.render('publication/editPublication', {
+       publication: {
+        title: publication.title, 
+        publishedDate: DateTime.local().toFormat('yyyy-MM-dd'),
+        link: publication.link, 
+        duration: publication.duration, 
+        preview: publication.preview, 
+      },
+       title: 'Edit Publication' 
+      }
+    )
   } catch (err) {
     console.error(err)
     res.status(500).send('Server Error')
