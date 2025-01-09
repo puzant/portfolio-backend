a3c9e2ed9ec70f7c80fb367aa62b10fe4fb4e66d
Puzant <pbakjejian@hotmail.com>
2025-01-09 13:24:09 +0400

    define project controller

---
+++ controllers/projectController.js
@@ -0,0 +1,11 @@
+import project from "../models/project.js"
+
+export const renderAddProject = async (req, res) => {}
+
+export const renderEditProject = async (req, res) => {}
+
+export const addProject = async (req, res) => {}
+
+export const editProject = async (req, res) => {}
+
+export const deleteProject = async (req, res) => {}
