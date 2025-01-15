da36c3a3013f139d0da330d26a6fd73c128f0064
Puzant <pbakjejian@hotmail.com>
2025-01-15 14:51:13 +0400

    configure multer

--- multerUpload.js
+++ multerUpload.js
@@ -7,8 +7,10 @@ const storage = multer.diskStorage({
   filename: (req, file, cb) => {
     cb(null, Date.now() + '-' + file.originalname);
   },
-});
+})
 
-const upload = multer({ storage: storage });
+const upload = multer({ 
+  storage: storage,
+});
 
 export default upload
