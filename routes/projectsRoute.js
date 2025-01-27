a57562573e6f3b3b788de8e371f526f3ed42670b
Puzant <pbakjejian@hotmail.com>
2025-01-27 23:30:05 +0200

    update projects route

--- routes/projectsRoute.js
+++ routes/projectsRoute.js
@@ -4,6 +4,8 @@ import upload from '../multerUpload.js'
 import {
   renderAddProject,
   renderEditProject,
+  getAllProjects,
+  getAllProjectsImages,
   addProject,
   editProject,
   deleteProject
@@ -12,6 +14,8 @@ import {
 const router = express.Router()
 
 //  API routes
+router.get('/api/', getAllProjects)
+router.get('/api/projects-images', getAllProjectsImages)
 router.post('/api/add', upload.single('preview'), addProject)
 router.post('/api/edit/:id', upload.single('preview'), editProject)
 router.delete('/api/delete/:id', deleteProject)
