55df8962b51d854fdebea16ced95869829665cb6
Puzant <pbakjejian@hotmail.com>
2025-02-01 22:40:13 +0200

    add new routes

--- routes/travelImagesRoute.js
+++ routes/travelImagesRoute.js
@@ -4,5 +4,7 @@ import { getAllTravelImages } from '../controllers/travelImagesController.js'
 const router = express.Router()
 
 router.get('/api', getAllTravelImages)
+router.post('/upload-image', getAllTravelImages)
+router.delete('/delete-image', getAllTravelImages)
 
 export default router
