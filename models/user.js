1f89e34824d500cbf9a1f9aeccb8fad12e95a0a2
Puzant <pbakjejian@hotmail.com>
2025-02-03 19:20:06 +0200

    create user modal

---
+++ models/user.js
@@ -0,0 +1,49 @@
+import mongoose from "mongoose"
+import bcrypt from 'bcryptjs';
+
+const UserSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true
+    },
+    password: {
+      type: String,
+      required: true
+    },
+    name: {
+      type: Date,
+      required: true
+    },
+    isActive: {
+      type: Boolean,
+      default: true
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    updatedAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  { timestamps: true }
+);
+
+UserSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next()
+
+  try {
+    const salt = await bcrypt.genSalt(10)
+    this.password = await bcrypt.hash(this.password, salt)
+    next()
+  } catch (err) {
+    next(err)
+  }
+})
+
+export default mongoose.model('User', UserSchema)
