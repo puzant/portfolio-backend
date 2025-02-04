1877d724c8e070974240be3aaa660f3b25566b5d
Puzant <pbakjejian@hotmail.com>
2025-02-04 20:49:07 +0200

    update auth routes

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,8 +1,11 @@
 import express from 'express'
-import { renderLogin } from '../controllers/authController.js'
+import { login, logout, createUser, renderLogin } from '../controllers/authController.js'
 
 const router = express.Router()
 
+router.post('/login', login)
 router.get('/login', renderLogin)
+router.post('/logout', logout)
+router.post('/create-user', createUser)
 
 export default router
