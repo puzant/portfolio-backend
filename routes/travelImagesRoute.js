759fc6ba57b2c7c2040fbace176704fb32344c50
Puzant <pbakjejian@hotmail.com>
2025-02-05 11:34:40 +0200

    make get travel images api public

--- routes/travelImagesRoute.js
+++ routes/travelImagesRoute.js
@@ -1,10 +1,12 @@
 import express from "express"
+
 import { getAllTravelImages } from '../controllers/travelImagesController.js'
+import authMiddleware from '../middlewares/authMiddleware.js'
 
 const router = express.Router()
 
 router.get('/api', getAllTravelImages)
-router.post('/upload-image', getAllTravelImages)
-router.delete('/delete-image', getAllTravelImages)
+router.post('/upload-image', authMiddleware, getAllTravelImages)
+router.delete('/delete-image', authMiddleware, getAllTravelImages)
 
 export default router
