1ef6a7cb9509cdda3b5f150005ce20a682ba2b4b
Puzant <pbakjejian@hotmail.com>
2025-02-12 22:03:51 +0200

    update user modal

--- models/user.js
+++ models/user.js
@@ -1,36 +1,15 @@
-import mongoose from "mongoose"
+import mongoose from 'mongoose'
 import bcrypt from 'bcrypt'
 
 const UserSchema = new mongoose.Schema(
   {
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-      lowercase: true,
-      trim: true
-    },
-    password: {
-      type: String,
-      required: true
-    },
-    name: {
-      type: String,
-      required: true
-    },
-    isActive: {
-      type: Boolean,
-      default: true
-    },
-    createdAt: {
-      type: Date,
-      default: Date.now,
-    },
-    updatedAt: {
-      type: Date,
-      default: Date.now,
-    },
-  },
+    email: { type: String, required: true, unique: true, lowercase: true, trim: true },
+    password: { type: String, required: true },
+    name: { type: String, required: true },
+    isActive: { type: Boolean, default: true },
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now },
+},
   { timestamps: true }
 );
 
