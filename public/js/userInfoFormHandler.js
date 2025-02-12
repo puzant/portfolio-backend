9ca8c7f965f46fb0f4e22adc5c18b37891e3bda6
Puzant <pbakjejian@hotmail.com>
2025-02-12 22:04:06 +0200

    update indentation

--- public/js/userInfoFormHandler.js
+++ public/js/userInfoFormHandler.js
@@ -1,29 +1,28 @@
 document.getElementById('loadingIndicator').style.display = 'none'
 
-    document.getElementById('userInfo').addEventListener('submit', async function(e) {
-      e.preventDefault()
-      document.getElementById('loadingIndicator').style.display = 'block'
+document.getElementById('userInfo').addEventListener('submit', async function(e) {
+  e.preventDefault()
+  document.getElementById('loadingIndicator').style.display = 'block'
 
-      const form = event.target
-      const formData = new FormData(form)
+  const form = e.target
+  const formData = new FormData(form)
 
-      try {
-        const response = await fetch(this.action, {
-          method: this.method,
-          body: JSON.stringify(Object.fromEntries(formData)),
-          headers: { "Content-Type": "application/json" }
-        })
-
-        const result = await response.json()
-        if (result.success) {
-          showAlert('success', result.message)
-        } else if (data.error) {
-          showAlert('danger', data.error)
-        }
-
-      } catch (err) {
-        showAlert('danger', 'An unexpected error occurred.')
-      } finally {
-        document.getElementById('loadingIndicator').style.display = 'none'
-      }
+  try {
+    const response = await axios({
+      url: this.action,
+      method: this.method,
+      data: Object.fromEntries(formData),
     })
+
+    if (response.data.success) {
+      showAlert('success', response.data.message)
+    } else if (response.data.error) {
+      showAlert('danger', response.data.error)
+    }
+  
+  } catch (err) {
+    showAlert('danger', 'An unexpected error occurred.')
+  } finally {
+    document.getElementById('loadingIndicator').style.display = 'none'
+  }
+})
