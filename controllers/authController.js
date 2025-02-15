045717c8012a3ec2af58e5abd56f4610b3ad8863
Puzant <pbakjejian@hotmail.com>
2025-02-15 22:19:05 +0200

    add refresh token logic

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,6 @@
 import User from '../models/user.js'
+import RefreshToken from '../models/refreshToken.js'
+
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
 
@@ -22,7 +24,24 @@ export const login = async (req, res) => {
       username: user.name,
       email: user.email
      }
+
     const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '1h' })
+    // const refreshToken = jwt.sign(payload, process.env.REFRESH_TOKEN_SECRET, { expiresIn: '7d' })
+
+    // const newRefreshToken = new RefreshToken({
+    //   refreshToken: refreshToken,
+    //   expiresAt: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+    //   userId: user._id
+    // })
+
+    // await newRefreshToken.save()
+
+    // res.cookie('refreshToken', refreshToken, {
+    //   httpOnly: true,
+    //   secure: process.env.NODE_ENV === 'production',
+    //   sameSite: 'strict',
+    //   maxAge: 7 * 24 * 60 * 60 * 1000
+    // })
 
     res.cookie('token', token, {
       httpOnly: true, 
@@ -41,6 +60,42 @@ export const login = async (req, res) => {
   }
 }
 
+export const refreshToken = (req, res) => {
+  try {
+    const refreshToken = req.cookies.refreshToken
+
+    if (!refreshToken) {
+      return res.status(403).json({ message: 'Refresh token is missing, please log in again' })
+    }
+
+    const decoded = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET)
+    const storedToken = Refresh.findOne({ refreshToken })
+
+    if (!storedToken) {
+      return res.status(403).json({ message: 'Refresh token not found in the database' })
+    }
+
+    const payload = { 
+      id: decoded._id,
+      username: decoded.name,
+      email: decoded.email
+     }
+
+     const newAccessToken = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '1hr' })
+
+     res.cookie('token', newAccessToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'strict',
+      maxAge: 3600000
+    })
+
+    return res.status(200).json({ message: 'Token refreshed successfully' });
+  } catch (err) {
+    return res.status(500).json({ message: 'Server error' });
+  }
+}
+
 export const logout = async (req, res) => {
   res.clearCookie('token', { httpOnly: true, secure: process.env.NODE_ENV === 'production', sameSite: 'strict' })
   res.status(200).json({ success: true, message: 'Logged out successfully' })
@@ -48,6 +103,14 @@ export const logout = async (req, res) => {
 
 export const changePassword = async (req, res) => {
   const { currentPassword, newPassword } = req.body
+}
+
+export const forgotPassword = async (req, res) => {
+  try {
+
+  } catch (err) {
+    
+  }
 }
 
 export const createUser = async (req, res) => {
