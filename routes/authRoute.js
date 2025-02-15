4ef23905fe42773dba8cf61e33ee3b9b7cc3eebd
Puzant <pbakjejian@hotmail.com>
2025-02-15 22:19:29 +0200

    add refresh token route

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { login, logout, createUser, renderLogin } from '../controllers/authController.js'
+import { login, logout, createUser, refreshToken, renderLogin } from '../controllers/authController.js'
 
 const router = express.Router()
 
@@ -7,6 +7,7 @@ const router = express.Router()
 router.post('/login', login)
 router.post('/logout', logout)
 router.post('/create-user', createUser)
+router.post('/refresh-token', refreshToken)
 
 // rendering routes
 router.get('/login', renderLogin)
