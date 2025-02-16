a11bafe371a799f1359117a93b74e26e587f1587
Puzant <pbakjejian@hotmail.com>
2025-02-16 23:46:48 +0200

    update settings route

--- routes/settingsRoute.js
+++ routes/settingsRoute.js
@@ -1,11 +1,12 @@
 import express from "express"
-import { updateUserInfo, updatePassword, renderSettings } from "../controllers/settingsController.js"
+import { updateUserInfo, updatePassword, deleteUserAccount, renderSettings } from "../controllers/settingsController.js"
 import authMiddleware from '../middlewares/authMiddleware.js'
 
 const router = express.Router()
 
 router.post('/update-user', authMiddleware, updateUserInfo)
 router.post('/update-password', authMiddleware, updatePassword)
+router.post('/delete-account', authMiddleware, deleteUserAccount)
 router.get('/', authMiddleware, renderSettings)
 
 export default router
