501e2405037cd22eee66935000d8d80b90a3bb31
Puzant <pbakjejian@hotmail.com>
2025-02-16 23:46:39 +0200

    redirect user to login page

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -4,7 +4,7 @@ const authMiddleware = (req, res, next) => {
   const token = req.cookies.token
   
   if (!token) {
-    return res.status(401).json({ message: 'Unauthorized: No token provided' })
+    return res.redirect('/auth/login')
   }
 
   try {
