37adc1fd51d4086241e12d65add91e4c6ba1320d
Puzant <pbakjejian@hotmail.com>
2025-02-21 15:24:35 +0200

    add cloudinary logic to remove / delete image

--- cloudinary.js
+++ cloudinary.js
@@ -1,28 +1,28 @@
 import { v2 as cloudinary } from 'cloudinary'
 
-
 export const fetchTravelImages = async () => {
-  try {
-    const result = await cloudinary.api.resources({
-      type: 'upload',
-      prefix: 'travels',
-      max_results: 50
-    })
+  const result = await cloudinary.api.resources({
+    type: 'upload',
+    prefix: 'travels',
+    max_results: 50
+  })
 
-    const webpImages = result.resources.map(resource => ({
-      url: cloudinary.url(resource.public_id, {
-        transformation: [
-          { width: 384, height: 288, crop: 'fill', format: 'webp', quality: 'auto' }
-        ],
-      }),
-      display_name: resource.display_name,
-      asset_id: resource.asset_id,
-      public_id: resource.public_id
-    }))
+  const webpImages = result.resources.map(resource => ({
+    url: cloudinary.url(resource.public_id, {
+      transformation: [
+        { width: 384, height: 288, crop: 'fill', format: 'webp', quality: 'auto' }
+      ],
+    }),
+    display_name: resource.display_name,
+    asset_id: resource.asset_id,
+    public_id: resource.public_id
+  }))
+  
+  return webpImages
+}
 
-    return webpImages
-    
-  } catch (err) {
-    throw new Error(`Cloudinary fetch failed: ${err.message}`);
-  }
+export const removeTravelImage = async (publicId) => {
+  const result = await cloudinary.uploader.destroy(publicId)
+  
+  return result
 }
