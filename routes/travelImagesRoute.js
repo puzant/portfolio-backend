9a37960415d4e5e7c99a6b473da5baf538a43c7f
Puzant <pbakjejian@hotmail.com>
2025-02-22 22:11:23 +0200

    change api routes

--- routes/travelImagesRoute.js
+++ routes/travelImagesRoute.js
@@ -2,12 +2,13 @@ import express from "express"
 
 import { getAllTravelImages, addTravelImage, deleteTravelImage, renderAddTravelImage } from '../controllers/travelImagesController.js'
 import authMiddleware from '../middlewares/authMiddleware.js'
+import upload from "../middlewares/multerUpload.js"
 
 const router = express.Router()
 
 router.get('/api', getAllTravelImages)
-router.post('/api/upload-image', authMiddleware, addTravelImage)
-router.delete('/api/delete-image', authMiddleware, deleteTravelImage)
+router.post('/api/add', authMiddleware, upload.single('travelImage'), addTravelImage)
+router.delete('/api/remove', authMiddleware, deleteTravelImage)
 router.get('/add', authMiddleware, renderAddTravelImage)
 
 export default router
