42d9fb048a30100c01ae638b1128eac80be11e82
Puzant <pbakjejian@hotmail.com>
2025-02-25 21:54:42 +0200

    instantiate and bind the publication controller class

--- routes/publicationsRoute.js
+++ routes/publicationsRoute.js
@@ -1,24 +1,19 @@
 import express from 'express'
-import { 
-  getAllPublications, 
-  addPublication, 
-  editPublication, 
-  deletePublication,
-  renderAddPublication,
-  renderEditPublication
-} from '../controllers/publicationController.js'
+import PublicationController from '../controllers/publicationController.js'
 import authMiddleware from '../middlewares/authMiddleware.js'
+import logger from '../logger.js'
 
 const router = express.Router()
+const publicationController = new PublicationController(logger)
 
 // API routes
-router.get('/api', getAllPublications)
-router.post('/api/add', authMiddleware, addPublication)
-router.post('/api/edit/:id', authMiddleware, editPublication)
-router.delete('/api/delete/:id', authMiddleware, deletePublication)
+router.get('/api', publicationController.getAllPublications.bind(publicationController))
+router.post('/api/add', authMiddleware, publicationController.addPublication.bind(publicationController))
+router.post('/api/edit/:id', authMiddleware, publicationController.editPublication.bind(publicationController))
+router.delete('/api/delete/:id', authMiddleware, publicationController.deletePublication.bind(publicationController))
 
 // rendering routes
-router.get('/add', authMiddleware, renderAddPublication)
-router.get('/edit/:id', authMiddleware, renderEditPublication)
+router.get('/add', authMiddleware, publicationController.renderAddPublication)
+router.get('/edit/:id', authMiddleware, publicationController.renderEditPublication)
 
 export default router
