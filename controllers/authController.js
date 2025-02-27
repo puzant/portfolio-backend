dbe689f28189229b2b7a3bf864aa56d80f92974d
Puzant <pbakjejian@hotmail.com>
2025-02-27 18:29:07 +0200

    add logger to auth controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -65,7 +65,8 @@ class AuthController {
         message: 'Login successful',
         user,
       })
-    } catch (err) {
+    } catch (error) {
+      this.logger.logError(error)
       return res.status(500).json({ message: 'Server error' })
     }
   }
@@ -101,7 +102,8 @@ class AuthController {
       })
   
       return res.status(200).json({ message: 'Token refreshed successfully' });
-    } catch (err) {
+    } catch (error) {
+      this.logger.logError(error)
       return res.status(500).json({ message: 'Server error' });
     }
   }
@@ -138,7 +140,8 @@ class AuthController {
       await newUser.save()
       res.status(201).json({ message: "User created successfully", user: newUser })
   
-    } catch (err) {
+    } catch (error) {
+      this.logger.logError(error)
       res.status(500).json({ message: "Server error", error: err });
     }
   }
