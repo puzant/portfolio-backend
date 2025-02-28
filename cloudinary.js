d98039cdb519398d04a7bfb646fa897ce675b0e9
Puzant <pbakjejian@hotmail.com>
2025-02-28 22:22:51 +0200

    change the implementation to class based

--- cloudinary.js
+++ cloudinary.js
@@ -1,36 +1,39 @@
 import { v2 as cloudinary } from 'cloudinary'
 
-export const fetchTravelImages = async () => {
-  const result = await cloudinary.api.resources({
-    type: 'upload',
-    prefix: 'travels',
-    max_results: 50
-  })
-
-  const webpImages = result.resources.map(resource => ({
-    url: cloudinary.url(resource.public_id, {
-      transformation: [
-        { width: 384, crop: 'fill', format: 'webp', quality: 'auto' }
-      ],
-    }),
-    display_name: resource.display_name,
-    asset_id: resource.asset_id,
-    public_id: resource.public_id
-  }))
+class CloudinaryService {
+  async fetchTravelImages() {
+    const result = await cloudinary.api.resources({
+      type: 'upload',
+      prefix: 'travels',
+      max_results: 50
+    })
   
-  return webpImages
+    const webpImages = result.resources.map(resource => ({
+      url: cloudinary.url(resource.public_id, {
+        transformation: [
+          { width: 384, crop: 'fill', format: 'webp', quality: 'auto' }
+        ],
+      }),
+      display_name: resource.display_name,
+      asset_id: resource.asset_id,
+      public_id: resource.public_id
+    }))
+    
+    return webpImages
+  }
+
+  async uploadTravelImage(filePath) {
+    const result = await cloudinary.uploader.upload(filePath, {
+      folder: 'travels',
+    })
+    return result
+  }
+
+  async removeTravelImage(publicId) {
+    const result = await cloudinary.uploader.destroy('travels/' + publicId)
+    return result
+  }
 }
 
-export const uploadTravelImage = async (filePath) => {
-  const result = await cloudinary.uploader.upload(filePath, {
-    folder: 'travels',
-  })
 
-  return result
-}
-
-export const removeTravelImage = async (publicId) => {
-  const result = await cloudinary.uploader.destroy('travels/' + publicId)
-  
-  return result
-}
+export default new CloudinaryService()
