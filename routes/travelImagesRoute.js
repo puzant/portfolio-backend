33a02e9bdb22531cfc82ef1e3835ee5224287108
Puzant <pbakjejian@hotmail.com>
2025-02-28 22:22:08 +0200

    pass cloudinary service as dependency

--- routes/travelImagesRoute.js
+++ routes/travelImagesRoute.js
@@ -3,10 +3,11 @@ import express from "express"
 import TravelImagesController from '../controllers/travelImagesController.js'
 import authMiddleware from '../middlewares/authMiddleware.js'
 import upload from "../middlewares/multerUpload.js"
+import cloudinaryService from '../cloudinary.js'
 import logger from '../logger.js'
 
 const router = express.Router()
-const travelImagesController = new TravelImagesController(logger)
+const travelImagesController = new TravelImagesController(logger, cloudinaryService)
 
 router.get('/api', travelImagesController.getAllTravelImages)
 router.post('/api/add', authMiddleware, upload.single('travelImage'), travelImagesController.addTravelImage)
