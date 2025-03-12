ec2101b67fbfbbfa14dd2d33401082a36b4365a1
Puzant <pbakjejian@hotmail.com>
2025-03-12 12:26:42 +0200

    implement not found error handler

---
+++ middlewares/notFoundHandler.js
@@ -0,0 +1,7 @@
+import AppError from "../appError.js"
+
+const notFoundHandler = (req, res, next) => {
+  next(new AppError(`Requested path ${req.originalUrl} not found`, 404));
+}
+
+export default notFoundHandler
