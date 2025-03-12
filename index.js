c11856510998dbd668d90c714c1e88b23de74093
Puzant <pbakjejian@hotmail.com>
2025-03-12 12:27:25 +0200

    organize and use middlewares

--- index.js
+++ index.js
@@ -4,27 +4,30 @@ import morgan from 'morgan'
 import dotenv  from 'dotenv'
 import express from 'express'
 import mongoose from 'mongoose'
-import { DateTime } from 'luxon'
 import { fileURLToPath } from 'url'
 import cookieParser from 'cookie-parser'
 import { v2 as cloudinary } from 'cloudinary'
 
 import { wakeupJob } from './cron.js'
-import cloudinaryService from './cloudinary.js'
-import Publications from './models/publications.js'
-import Projects from './models/project.js'
 
 //  API Routes
+import cmsRoute from './routes/cmsRoute.js'
+import authRoute from './routes/authRoute.js'
 import projectsRoute from './routes/projectsRoute.js'
+import settingsRoute from './routes/settingsRoute.js'
 import publicationsRoute from './routes/publicationsRoute.js'
 import travelImagesRoute from './routes/travelImagesRoute.js'
-import authRoute from './routes/authRoute.js'
-import settingsRoute from './routes/settingsRoute.js'
 
-import AuthMiddleware from './middlewares/authMiddleware.js'
+import errorHandler from './middlewares/errorMiddleware.js'
+import notFoundHandler from './middlewares/notFoundHandler.js'
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+})
 
 dotenv.config()
-
 const app = express()
 const PORT = process.env.PORT
 const MONGO_URI = process.env.MONGO_URI
@@ -46,40 +49,16 @@ app.use(cors({
 }))
 
 //  Register routes
+app.use(cmsRoute)
 app.use('/publications', publicationsRoute)
 app.use('/projects', projectsRoute)
 app.use('/travel-images', travelImagesRoute)
 app.use('/auth', authRoute)
 app.use('/settings', settingsRoute)
+app.use("*", notFoundHandler)
+app.use(errorHandler)
 
 wakeupJob.start()
-
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
-
-app.get('/cms', AuthMiddleware, async (req, res) => {
-  const projects = await Projects.find({}).lean()
-  const publications = await Publications.find({}).sort({ publishedDate: -1 }).lean()
-  const webpImages = await cloudinaryService.fetchTravelImages()
-
-  const tranformedPublications = publications.map(p => {
-    return {
-      ...p, 
-      publishedDate: DateTime.fromJSDate(p.publishedDate).toFormat('yyyy-MM-dd')
-    }
-  })
-
-  res.render('index', {
-    travelImages: webpImages,
-    publications: tranformedPublications,
-    projects: projects,
-    title: 'CMS',
-    user: req.user
-  })
-})
 
 mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
