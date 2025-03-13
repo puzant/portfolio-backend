6487dc206ed6b7507d3932cec7eda649a82bb1de
Puzant <pbakjejian@hotmail.com>
2025-03-13 22:54:32 +0200

    add new methods for project service

--- services/projectService.js
+++ services/projectService.js
@@ -1,10 +1,19 @@
 import { v2 as cloudinary } from 'cloudinary'
 import Project from "../models/project.js"
 import AppError from '../appError.js'
+import mongoose from 'mongoose'
 
 class ProjectService {
   async getAll() {
     return Project.find().lean()
+  }
+
+  async getById(id) {
+    const project = await Project.findById(id)
+    
+    if (!project) 
+      throw new AppError("Project was not found", 404)
+    return project
   }
 
   async fetchProjectImages() {
@@ -42,13 +51,49 @@ class ProjectService {
     return project
   }
 
-  async editProject() {
+  async editProject(reqBody, id, filePath) {
+    const { name, description, preview, link, public_id, asset_id, previewChanged } = reqBody
 
+    if (!name || !description || !link) throw new AppError("All fields are required: name, description, link", 400)
+
+    if (!mongoose.Types.ObjectId.isValid(id))
+      throw new AppError("Invalid Project ID", 400)
+
+    let updatedFields = { name, description, link };
+    const isPreviewChanged = (previewChanged === 'true')
+
+    if (isPreviewChanged) {
+     try {
+      await cloudinary.uploader.destroy(public_id)
+      const uploadingResult =  await cloudinary.uploader.upload(filePath, { folder: 'projects' })
+
+      updatedFields.preview = uploadingResult.secure_url;
+      updatedFields.public_id = uploadingResult.public_id;
+      updatedFields.asset_id = uploadingResult.asset_id;
+     } catch (error) {
+      throw new AppError(`Cloudinary Error ${error.message}`, 500)
+     }
+    } else {
+      updatedFields.preview = preview
+      updatedFields.asset_id = asset_id
+      updatedFields.public_id = public_id
+    }
+
+    const updatedProject = await Project.findByIdAndUpdate(id, updatedFields, { new: true, runValidators: true });
+    if (!updatedProject) 
+      throw new AppError("Project was not found", 404)
+    return updatedProject
   }
 
-  async deleteProject() {
-    
+  async deleteProject(publicId, id) {
+    if (!publicId) throw new AppError("Project public ID is required", 400)
+
+    const projectToDelete = await Project.findById(id)
+    if (!projectToDelete) throw new AppError("Project not found", 404)
+
+    await cloudinary.uploader.destroy(publicId)
+    await Project.findByIdAndDelete(id)
   }
 }
 
-export default new ProjectService()
+export default ProjectService
