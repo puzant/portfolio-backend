8384d92b2f569ebae5b249dfcf0d7deb356ef7e5
Puzant <pbakjejian@hotmail.com>
2025-03-24 22:40:27 +0200

    delete old files

--- routes/cmsRoute.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import CmsController from '@controllers/cms.controller.js'
-import authMiddleware from '@middlewares/authMiddleware.js'
-
-const router = express.Router()
-
-router.get('/cms', authMiddleware, CmsController)
-
-export default router
