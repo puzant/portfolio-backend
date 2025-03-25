2509f608667abf610104cadc73aaebba927daf1d
Puzant <pbakjejian@hotmail.com>
2025-03-25 11:29:24 +0200

    organize imports

--- routes/renderRoutes.js
+++ routes/renderRoutes.js
@@ -1,25 +1,24 @@
 import express from "express"
 import authMiddleware from "@middlewares/authMiddleware.js"
-import PublicationController from "@controllers/publication.controller.js"
-import ProjectController from "@controllers/project.controller.js"
 import AuthController from "@controllers/auth.controller.js"
+import ProjectController from "@controllers/project.controller.js"
 import SettingsController from "@controllers/settings.controller.js"
+import PublicationController from "@controllers/publication.controller.js"
 
 import ProjectService from "@services/projectService.js"
-import PublicationService from "@services/publicationService.js"
 import SettingsService from "@services/settingsService.js"
+import PublicationService from "@services/publicationService.js"
 
 const router = express.Router()
 
-const publicationService = new PublicationService()
-const publicationController = new PublicationController(publicationService)
 const authController = new AuthController()
-
 const projectService = new ProjectService()
-const projectController = new ProjectController(projectService)
-
 const settingsService = new SettingsService()
+const publicationService = new PublicationService()
+
+const projectController = new ProjectController(projectService)
 const settingsController = new SettingsController(settingsService)
+const publicationController = new PublicationController(publicationService)
 
 router.get('/publication/add', authMiddleware, publicationController.renderAddPublication)
 router.get('/publication/edit/:id', authMiddleware, publicationController.renderEditPublication)
