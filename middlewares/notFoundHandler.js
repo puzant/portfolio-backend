25afed45d5368b4db57b5ca386edb280f3f2e24b
Puzant <pbakjejian@hotmail.com>
2025-03-26 14:36:49 +0200

    update path

--- middlewares/notFoundHandler.js
+++ middlewares/notFoundHandler.js
@@ -1,4 +1,4 @@
-import AppError from "../utils/appError.js"
+import AppError from "#utils/appError.js"
 
 const notFoundHandler = (req, res, next) => {
   next(new AppError(`Requested path ${req.originalUrl} not found`, 404));
