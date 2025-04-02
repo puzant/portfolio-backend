be6e2bcc7e21292b518650a079891fee50b99300
Puzant <pbakjejian@hotmail.com>
2025-04-02 21:29:54 +0300

    rename to .middleware

---
+++ middlewares/error.middleware.js
@@ -0,0 +1,17 @@
+const errorMiddleware = (err, req, res, next) => {
+  const statusCode = err.statusCode || 500
+  const message = err.message || 'Internal server error'
+  const type = err.type || 'general'
+  const errorDate = err.timeStamp
+  const errors = err.errors || []
+
+  res.status(statusCode).json({
+    success: false,
+    message,
+    type,
+    errorDate,
+    errors
+  })
+}
+
+export default errorMiddleware
