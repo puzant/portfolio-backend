adebfb4efbdc6b647b36bb456785f4940b17b275
Puzant <pbakjejian@hotmail.com>
2025-04-02 21:29:32 +0300

    delete old files

--- middlewares/notFoundHandler.js
+++
@@ -1,7 +0,0 @@
-import AppError from "#utils/appError.js"
-
-const notFoundHandler = (req, res, next) => {
-  next(new AppError(`Requested path ${req.originalUrl} not found`, 404));
-}
-
-export default notFoundHandler
