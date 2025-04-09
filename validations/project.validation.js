93c18d63c92cad387ecbc3cd73efd2aa549fd0d1
Puzant <pbakjejian@hotmail.com>
2025-04-09 20:35:12 +0300

    create project validation

--- validations/project.validation.js
+++ validations/project.validation.js
@@ -1,2 +1,34 @@
-import { body } from "express-validator"
+import { body, check } from "express-validator"
+import { StatusCodes as Status } from "http-status-codes"
+import AppError from "#utils/appError.js"
 
+const projectValidation = [
+  body("name")
+    .exists({ checkFalsy: true })
+    .withMessage("Name field is required")
+    .bail()
+    .isString()
+    .withMessage("Name should be string"),
+  body("description")
+    .exists({ checkFalsy: true })
+    .withMessage("Description field is required")
+    .bail()
+    .isString()
+    .withMessage("Description should be string"),
+  body("link")
+    .exists({ checkFalsy: true })
+    .withMessage("Link field is required")
+    .bail()
+    .isString()
+    .withMessage("Link should be string"),
+  check("file")
+    .custom((value, { req,  }) => {
+      if (!req.file || !req.file.path) {
+        throw new AppError("Preview is required", Status.BAD_REQUEST)
+      }
+
+      return true
+    })
+]
+
+export default projectValidation
