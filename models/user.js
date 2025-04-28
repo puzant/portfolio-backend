655b92c1690b5d27ee1b1935f2ed83ea27ecbc94
Puzant <pbakjejian@hotmail.com>
2025-04-28 11:32:53 +0300

    add new fields in the user modal

--- models/user.js
+++ models/user.js
@@ -6,6 +6,8 @@ const UserSchema = new mongoose.Schema(
     email: { type: String, required: true, unique: true, lowercase: true, trim: true },
     password: { type: String, required: true },
     name: { type: String, required: true },
+    country: { type: String, required: true },
+    city: { type: String, required: true },
     isActive: { type: Boolean, default: true },
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date, default: Date.now },
