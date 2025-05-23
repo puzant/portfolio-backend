7b594a2c0a7bc4d8890c4c1f973e468318579120
Puzant <pbakjejian@hotmail.com>
2025-05-23 17:13:17 +0300

    wrap fetchTravelImages in try / catch

--- cloudinary.js
+++ cloudinary.js
@@ -1,25 +1,31 @@
+import AppError from '#utils/appError.js'
 import { v2 as cloudinary } from 'cloudinary'
+import { StatusCodes as Status } from 'http-status-codes'
 
 class CloudinaryService {
   async fetchTravelImages() {
-    const result = await cloudinary.api.resources({
-      type: 'upload',
-      prefix: 'travels',
-      max_results: 50
-    })
-  
-    const webpImages = result.resources.map(resource => ({
-      url: cloudinary.url(resource.public_id, {
-        transformation: [
-          { width: 384, crop: 'fill', format: 'webp', quality: 'auto' }
-        ],
-      }),
-      display_name: resource.display_name,
-      asset_id: resource.asset_id,
-      public_id: resource.public_id
-    }))
-    
-    return webpImages
+    try {
+      const result = await cloudinary.api.resources({
+        type: 'upload',
+        prefix: 'travels',
+        max_results: 50
+      })
+
+      const webpImages = result.resources.map(resource => ({
+        url: cloudinary.url(resource.public_id, {
+          transformation: [
+            { width: 384, crop: 'fill', format: 'webp', quality: 'auto' }
+          ],
+        }),
+        display_name: resource.display_name,
+        asset_id: resource.asset_id,
+        public_id: resource.public_id
+      }))
+
+      return webpImages
+    } catch (error) {
+      throw new AppError(err.message, Status.INTERNAL_SERVER_ERROR)
+    }
   }
 
   async uploadTravelImage(filePath) {
