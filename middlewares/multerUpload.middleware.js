527f5ab34d3b157055b984ad214fe210f55cbacb
Puzant <pbakjejian@hotmail.com>
2025-06-13 20:07:07 +0300

    create uploads folder if it doesn't exists

--- middlewares/multerUpload.middleware.js
+++ middlewares/multerUpload.middleware.js
@@ -1,4 +1,12 @@
 import multer from 'multer'
+import fs from 'fs'
+import path from 'path'
+
+const uploadDir = path.resolve(process.cwd(), 'uploads');
+
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true })
+}
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
