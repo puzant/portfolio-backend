98e70b5179d2c1c9de686c171a9224a6ef069d3b
Puzant <PBAKJEJIAN@hotmail.com>
2025-09-11 11:15:23 +0300

    rename to .model prefix

---
+++ models/project.model.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose"
+
+const projectSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  preview: { type: String, required: true },
+  link: { type: String, required: true },
+  public_id: { type: String, required: true },
+  asset_id: { type: String, required: true },
+  priority: { type: Number, default: 0 }
+})
+
+export default mongoose.model('Porject', projectSchema)
