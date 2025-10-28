cf45d1f74788f6c682fba438cf7e9dddb075e097
Puzant <PBAKJEJIAN@hotmail.com>
2025-10-28 16:11:45 +0200

    commit generated migration

---
+++ migrations/20251028105845891-project.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose"
+import project from "../models/project.model.js"
+
+export async function up() {
+  await mongoose.connect(process.env.MONGO_URI)
+  await project.updateMany({ wactive: { $exists: false } }, { $set: { active: true } });
+  console.log("✅ Migration add-active-field-for-properties applied")
+  await mongoose.disconnect()  
+}
+
+export async function down() {
+  await mongoose.connect(process.env.MONGO_URI)
+  await project.updateMany({}, { $unset: { active: "" } })
+  console.log("⏪ Migration add-active-field-for-properties reverted")
+  await mongoose.disconnect()
+}
