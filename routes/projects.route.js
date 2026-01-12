5bb00c58778be065242d3e830a51e2ae53d5d970
Puzant <pbakjejian@hotmail.com>
2026-01-12 18:54:29 +0300

    restrict project images to admin

--- routes/projects.route.js
+++ routes/projects.route.js
@@ -12,10 +12,10 @@ const projectService = new ProjectService(cache)
 const projectController = new ProjectController(projectService)
 
 router.get('/', projectController.getAllProjects)
-router.get('/projects-images', authMiddleware, projectController.getAllProjectsImages)
+router.get('/projects-images', authMiddleware, restrictTo('admin'), projectController.getAllProjectsImages)
 router.post('/add', authMiddleware, restrictTo('admin'), upload.single('preview'), projectValidation, projectController.addProject)
 router.post('/edit/:id', authMiddleware, restrictTo('admin'), upload.single('preview'), projectValidation, projectController.editProject)
-router.post('/reorder-project', authMiddleware, restrictTo('admin'), projectController.reorderProject)
+router.patch('/reorder-project', authMiddleware, restrictTo('admin'), projectController.reorderProject)
 router.delete('/delete/:id', authMiddleware, restrictTo('admin'), projectController.deleteProject)
 
 export default router
