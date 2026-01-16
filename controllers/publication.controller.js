cfd8027d331b08e4491d3b671b1472e5458278da
Puzant <pbakjejian@hotmail.com>
2026-01-16 14:49:13 +0300

    convert publication controller methods to arrow functions

--- controllers/publication.controller.js
+++ controllers/publication.controller.js
@@ -5,45 +5,39 @@ import ApiResponse from '#utils/apiResponse.js'
 class PublicationController {
   constructor(publicationService) {
     this.publicationService = publicationService
-    this.getAllPublications = asyncHandler(this.getAllPublications.bind(this))
-    this.addPublication = asyncHandler(this.addPublication.bind(this))
-    this.editPublication = asyncHandler(this.editPublication.bind(this))
-    this.deletePublication = asyncHandler(this.deletePublication.bind(this))
-    this.renderAddPublication = asyncHandler(this.renderAddPublication.bind(this))
-    this.renderEditPublication = asyncHandler(this.renderEditPublication.bind(this))
   }
 
-  async getAllPublications (req, res) {
+  getAllPublications = asyncHandler(async (req, res) => {
     const publications = await this.publicationService.getAll()
     return res.status(Status.OK).json(ApiResponse.successResponse("Publications retrived successfully", publications))
-  }
+  })
 
-  async addPublication(req, res) {  
+  addPublication = asyncHandler(async (req, res) => {  
     const publication = await this.publicationService.addPublication(req)
     return res.status(Status.CREATED).json(ApiResponse.successResponse("Publication saved successfully", publication ))
-  }
+  })
 
-  async editPublication(req, res) {
+  editPublication = asyncHandler(async (req, res) => {
     const { id } = req.params
     const updatedPublication = await this.publicationService.editPublication(req, id)
 
     return res.status(Status.OK).json(ApiResponse.successResponse("Publication updated successfully", updatedPublication))
-  }
+  })
 
-  async deletePublication(req, res) {
+  deletePublication = asyncHandler(async (req, res) => {
     await this.publicationService.deletePublication(req.params.id)
     return res.status(Status.OK).json(ApiResponse.successResponse("Publication deleted successfully"))
-  }
+  })
 
   // Route to render the Add Publication form
-  async renderAddPublication(req, res) {
+  renderAddPublication = asyncHandler(async (req, res) => {
     return res.render('publications/addPublication', {
       title: 'Add Publication',
     })
-  }
+  })
 
   // Route to render the Edit Publication form
-  async renderEditPublication (req, res) {
+  renderEditPublication = asyncHandler(async (req, res) => {
     const publication = await this.publicationService.getById(req.params.id)
     const formattedPublication = this.publicationService.formatPublicationData(publication)
       
@@ -51,7 +45,7 @@ class PublicationController {
       publication: formattedPublication,
       title: 'Edit Publication',
     })
-  }
+  })
 }
 
 export default PublicationController
