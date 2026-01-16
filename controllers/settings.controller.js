d19851f85675751123dd193036874ca48d760a75
Puzant <pbakjejian@hotmail.com>
2026-01-16 14:49:39 +0300

    convert settings controller methods to arrow functions

--- controllers/settings.controller.js
+++ controllers/settings.controller.js
@@ -6,34 +6,29 @@ import User from '#models/user.model.js'
 class SettingsController {
   constructor(settingsService) {
     this.settingsService = settingsService
-    this.triggerNetlifyDeployment = asyncHandler(this.triggerNetlifyDeployment.bind(this))
-    this.updateUserInfo = asyncHandler(this.updateUserInfo.bind(this))
-    this.deleteUserAccount = asyncHandler(this.deleteUserAccount.bind(this))
-    this.updatePassword = asyncHandler(this.updatePassword.bind(this))
-    this.renderSettings = this.renderSettings.bind(this)
   }
 
-  async triggerNetlifyDeployment(req, res) {
+  triggerNetlifyDeployment = asyncHandler(async (req, res) => {
     const response = await this.settingsService.handleNetlifyDeployment()
     return res.status(Status.OK).json(ApiResponse.successResponse("Deployment started", response))
-  }
+  })
 
-  async updateUserInfo(req, res) {
+  updateUserInfo = asyncHandler(async (req, res) => {
     const updatedUser = await this.settingsService.updateUserInfo(req)
     return res.status(Status.OK).json(ApiResponse.successResponse("User updated successfully", updatedUser))
-  }
+  })
 
-  async deleteUserAccount(req, res) {
+  deleteUserAccount = asyncHandler(async (req, res) => {
     const user = await this.settingsService.deleteUserAccount(req)
     return res.status(Status.OK).json(ApiResponse.successResponse("User deleted successfully", user))
-  }
+  })
 
-  async updatePassword(req, res) {
+  updatePassword = asyncHandler(async (req, res) => {
      await this.settingsService.updatePassword(req)
     return res.status(200).json(ApiResponse.successResponse("Password updated successfully"))
-  }
+  })
 
-  async renderSettings(req, res) {
+  renderSettings = asyncHandler(async (req, res) => {
     const user = await User.findById(req.user.id)
 
     try {
@@ -44,7 +39,7 @@ class SettingsController {
     } catch (error) {
       res.status(500).render('error', { message: 'Internal Server Error. Please try again later.' })
     }
-  }
+  })
 }
 
 export default SettingsController
