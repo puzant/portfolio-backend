62205222325388355a39a9d5803abef8c128f012
Puzant <pbakjejian@hotmail.com>
2026-03-08 19:44:56 +0300

    settings controller for cache toggle

--- controllers/settings.controller.js
+++ controllers/settings.controller.js
@@ -1,7 +1,6 @@
 import asyncHandler from 'express-async-handler'
 import { StatusCodes as Status } from 'http-status-codes'
 import ApiResponse from '#utils/apiResponse.js'
-import User from '#models/user.model.js'
 
 class SettingsController {
   constructor(settingsService) {
@@ -11,6 +10,11 @@ class SettingsController {
   triggerNetlifyDeployment = asyncHandler(async (req, res) => {
     const response = await this.settingsService.handleNetlifyDeployment()
     return res.status(Status.OK).json(ApiResponse.successResponse("Deployment started", response))
+  })
+
+  toggleCache = asyncHandler(async (req, res) => {
+    await this.settingsService.toggleCache(req)
+    return res.status(Status.OK).json(ApiResponse.successResponse("Cache settings updated"))
   })
 
   updateUserInfo = asyncHandler(async (req, res) => {
@@ -29,16 +33,9 @@ class SettingsController {
   })
 
   renderSettings = asyncHandler(async (req, res) => {
-    const user = await User.findById(req.user.id)
-
-    try {
-      res.render('settings/settings', {
-        title: 'Settings',
-        user: user
-      })
-    } catch (error) {
-      res.status(500).render('error', { message: 'Internal Server Error. Please try again later.' })
-    }
+    res.render('settings/settings', {
+      title: 'Settings',
+    })
   })
 }
 
