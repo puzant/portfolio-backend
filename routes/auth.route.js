b5c04f5decae44f6fd5f857cdd1202818d77740c
Puzant <pbakjejian@hotmail.com>
2026-03-16 01:43:21 +0300

    create new instance of email service and inject it

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,11 +1,13 @@
 import express from 'express'
 import AuthController from '#controllers/auth.controller.js'
 import AuthService from '#services/auth.service.js'
+import EmailService from '#services/email.service.js'
 import authValidation from '#validations/auth.validation.js'
 import authMiddleware from '#middlewares/auth.middleware.js'
 
 const router = express.Router()
-const authService = new AuthService()
+const emailService = new EmailService()
+const authService = new AuthService(emailService)
 const authController = new AuthController(authService)
 const { loginValidation, createUserValidation } = authValidation
 
