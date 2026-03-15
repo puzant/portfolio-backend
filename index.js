869c4f9548594d348c3cd1c8db1bbc9e40b53a8c
Puzant <pbakjejian@hotmail.com>
2026-03-16 01:43:04 +0300

    import and configure redis connection

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import cors from 'cors'
 import path from 'path'
 import morgan from 'morgan'
-import dotenv  from 'dotenv'
+
 import express from 'express'
 import mongoose from 'mongoose'
 import { fileURLToPath } from 'url'
@@ -10,6 +10,7 @@ import { v2 as cloudinary } from 'cloudinary'
 
 import { keepAliveJob } from './jobs/keepAlive.js'
 import { cacheWarmerJob } from './jobs/cacheWarmer.js'
+import RediSConnection from './config/redis.js'
 
 //  API Routes
 import authRoute from './routes/auth.route.js'
@@ -29,12 +30,13 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 })
 
-dotenv.config()
 const app = express()
 const PORT = process.env.PORT
 const MONGO_URI = process.env.MONGO_URI
 const __fileName = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__fileName)
+const redisConnection = new RediSConnection()
+const redis = redisConnection.getConnection()
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
@@ -66,8 +68,16 @@ keepAliveJob.start()
 cacheWarmerJob.start()
 
 mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.error('MongoDB connection error:', err))
+  .then(() => console.log('✅ Connected to MongoDB'))
+  .catch(err => console.error('❌ MongoDB connection error:', err))
+
+redis.on('connect', () => {
+  console.log('✅ Connected to Redis');
+})
+
+redis.on('error', (err) => {
+  console.error('❌ Redis connection error:', err);
+})
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
