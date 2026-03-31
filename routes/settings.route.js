f62517d2d25ca9523c95b0c522d0614b72f41993
Puzant <pbakjejian@hotmail.com>
2026-03-31 15:32:42 +0300

    new api to update reorder toggle

--- routes/settings.route.js
+++ routes/settings.route.js
@@ -12,5 +12,6 @@ router.post('/update-user', authMiddleware, settingsController.updateUserInfo)
 router.post('/update-password', authMiddleware, settingsController.updatePassword)
 router.post('/delete-account', authMiddleware, settingsController.deleteUserAccount)
 router.patch('/cache-toggles', authMiddleware, settingsController.toggleCache)
+router.patch('/reorder-toggle', authMiddleware, settingsController.toggleReOrder)
 
 export default router
